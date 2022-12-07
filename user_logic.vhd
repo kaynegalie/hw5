43ce6be38dfff895089eef5f1e081e2e03d1a06e
kaynegalie <115092041+kaynegalie@users.noreply.github.com>
2022-12-07 08:17:19 -0500

    Update user_logic.vhd

--- user_logic.vhd
+++ user_logic.vhd
@@ -1,3 +1,12 @@
+ibrary IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VComponents.all;
+ 
+library UNISIM;
+use UNISIM.VComponents.all;
+
+
 entity user_logic is
   PORT (aclk : IN STD_LOGIC;
      aresetn : IN STD_LOGIC;
@@ -7,3 +16,33 @@ entity user_logic is
            z : OUT STD_LOGIC_VECTOR(9 DOWNTO 0)
   );
 end user_logic;
+
+  
+   architecture Behavioral of user_logic2 is
+     
+     
+     
+
+COMPONENT cordic_0
+  PORT (
+    aclk : IN STD_LOGIC;
+    s_axis_cartesian_tvalid : IN STD_LOGIC;
+    s_axis_cartesian_tdata : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+    m_axis_dout_tvalid : OUT STD_LOGIC;
+    m_axis_dout_tdata : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+  );
+END COMPONENT;
+begin
+  
+  signal temp : signed(15 downto 0);
+     temp <= a+b
+U : cordic_0
+  PORT MAP (
+    aclk => aclk,
+    s_axis_cartesian_tvalid => din_tvalid,
+    s_axis_cartesian_tdata => temp,
+    m_axis_dout_tvalid => dout_tvalid,
+    m_axis_dout_tdata => z
+  );
+ 
+end Behavioral;
